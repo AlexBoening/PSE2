e4dd58812ae60b6270cb4b0b21155cbec4ccd1dd
NilsWeidmann <nils-w92@web.de>
2013-11-27 13:38:04 +0100

    SecondName war noch nicht überall weg...

--- PSE2 - icash/src/server_neu/RestResource.java
+++ PSE2 - icash/src/server_neu/RestResource.java
@@ -243,7 +243,7 @@ public class RestResource {
 	@Path("/s/createCustomer")
 	@Produces({ MediaType.TEXT_PLAIN + "; charset=utf-8" })
 	public Response createCustomer(@FormParam("firstName")String firstName, 
-			                       @FormParam("secondName") String secondName, 
+			                       @FormParam("lastName") String lastName, 
 			                       @FormParam("password") String password, 
 			                       @Context HttpServletRequest req) {
 		
@@ -251,10 +251,10 @@ public class RestResource {
 		Logger logger = Logger.getRootLogger();
 		logger.info(new java.util.Date() + ": IP: " + req.getRemoteAddr());
 		logger.info(new java.util.Date() + ": Method: /s/createCustomer");
-		logger.info(new java.util.Date() + ": first name: " + firstName + "/ second name: " + secondName);
+		logger.info(new java.util.Date() + ": first name: " + firstName + "/  name: " + lastName);
 		
 		try {
-			Customer c = new Customer(firstName, secondName, password);
+			Customer c = new Customer(firstName, lastName, password);
 			JSONObject jo = new JSONObject();
 			jo.put("id", c.getId());
 			// Customer was created successfully
@@ -569,7 +569,7 @@ public class RestResource {
 
 				jo.put("id", a.getId());
 				jo.put("firstName", a.getFirstName());
-				jo.put("secondName", a.getLastName());
+				jo.put("lastName", a.getLastName());
 				
 				JSONArray ja = new JSONArray();
 				Account[] acc = new Account[a.getAccounts().size()];
@@ -587,7 +587,7 @@ public class RestResource {
 					JSONObject customer = new JSONObject();
 					customer.put("id", acc[i].getCustomer().getId());
 					customer.put("firstName", acc[i].getCustomer().getFirstName());
-					customer.put("secondName", acc[i].getCustomer().getLastName());
+					customer.put("lastName", acc[i].getCustomer().getLastName());
 					currentAccount.put("customer", customer);
 					
 					JSONObject accountType = new JSONObject();
@@ -658,7 +658,7 @@ public class RestResource {
 				jo.put("banks", banks);
 				
 				// Customers
-				String[] columnCustomer = {"idCustomer", "firstNameCustomer", "SecondNameCustomer"};
+				String[] columnCustomer = {"idCustomer", "firstNameCustomer", "lastNameCustomer"};
 				table = "Customer";
 				
 				value = SQL.select(columnCustomer, table, condition, connector);
@@ -667,14 +667,14 @@ public class RestResource {
 					JSONObject customer = new JSONObject();
 					customer.put("id", Convert.toInt(value[i][0]));
 					customer.put("firstName", value[i][1]);
-					customer.put("secondName", value[i][2]);
+					customer.put("lastName", value[i][2]);
 					customers.put(customer);
 				}
 				
 				jo.put("customers", customers);
 				
 				// Admins
-				String[] columnAdmin = {"idAdministrator", "firstNameAdministrator", "SecondNameAdministrator"};
+				String[] columnAdmin = {"idAdministrator", "firstNameAdministrator", "lastNameAdministrator"};
 				table = "Administrator";
 				
 				value = SQL.select(columnAdmin, table, condition, connector);
@@ -683,7 +683,7 @@ public class RestResource {
 					JSONObject admin = new JSONObject();
 					admin.put("id", Convert.toInt(value[i][0]));
 					admin.put("firstName", value[i][1]);
-					admin.put("secondName", value[i][2]);
+					admin.put("lastName", value[i][2]);
 					admins.put(admin);
 				}
 				

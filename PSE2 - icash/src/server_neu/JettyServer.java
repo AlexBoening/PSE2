80c23054b106924c83469154c9722da4e9900383
NilsWeidmann <nils-w92@web.de>
2013-11-28 15:12:47 +0100

    Logging

--- PSE2 - icash/src/server_neu/JettyServer.java
+++ PSE2 - icash/src/server_neu/JettyServer.java
@@ -5,10 +5,10 @@ package server_neu;
 import classes.SQL;
 
 import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.SimpleLayout;
-
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerCollection;
@@ -16,17 +16,21 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+import classes.*;
+import client.*;
+
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 import java.sql.*;
+import java.util.logging.*;
 
 public class JettyServer {
     
 	public static boolean securityMode;
 	
-	public static void main(String args[]) throws Exception {
+	public static void main(String[] args) throws Exception {
 		Server server = new Server(9998);
 		securityMode = false;
 		
@@ -35,10 +39,12 @@ public class JettyServer {
         SimpleLayout layout = new SimpleLayout();
         ConsoleAppender appender = new ConsoleAppender(layout);
         logger.addAppender(appender);
+        FileAppender fa = new FileAppender(layout, "log.txt");
+		logger.addAppender(fa);
         logger.setLevel(Level.ALL);
         
 		// JERSEY
-		ResourceConfig rc = new PackagesResourceConfig("server_neu");
+		ResourceConfig rc = new PackagesResourceConfig("server");
 		ServletContextHandler sh = new ServletContextHandler();
 		sh.setContextPath("/rest");
 		sh.addServlet(new ServletHolder(new ServletContainer(rc)), "/*");
@@ -57,8 +63,6 @@ public class JettyServer {
 		try {
 			SQL.getConnection();  
 			logger.info(new java.util.Date() + ": Server started");
-			//TestClient tc = new TestClient();
-			//Account a = tc.getAccount(1);
 		}
 		catch (SQLException e) {
 			logger.error(new java.util.Date() + ": Database connection failed!");

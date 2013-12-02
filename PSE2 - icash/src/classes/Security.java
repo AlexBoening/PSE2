77e89cb3d85953c801fd5f90cfc279d1c0140c07
NilsWeidmann <nils-w92@web.de>
2013-12-02 15:15:52 +0100

    Security Neu

--- PSE2 - icash/src/classes/Security.java
+++ PSE2 - icash/src/classes/Security.java
@@ -1,31 +1,40 @@
 package classes;
 
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
+//import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.log4j.Logger;
 
 public class Security {
 
 	public static void main(String[] args) {
-		String password = "myPassword";
-		createPasswordHash(password);
+		String password = "geheim";
+		password = createPasswordHash(password);
 		System.out.println(password);
 	}
 	
 	public static String createPasswordHash(String password) {
-        try {
-        	  MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+       // try {
+        	  /*MessageDigest sha1 = MessageDigest.getInstance("SHA1");
         	  String hash = new String (sha1.digest(password.getBytes()));
-        	  hash.replace("\'", "!");
-        	  hash.replace("\"", "!");
-        	  password = hash;
-        	 
-        	} catch (NoSuchAlgorithmException ex) {
-        		Logger logger = Logger.getRootLogger();
-        		logger.info(new java.util.Date() + "An error occured during the creation of the password-hash");
+        	  if (URLEncode)
+        		  password = URLEncoder.encode(hash, "UTF-8");
+        	  else
+        		  password = hash;*/
+        	return DigestUtils.sha1Hex(password);
+        	/*} 
+        catch (NoSuchAlgorithmException ex) {
+        	Logger logger = Logger.getRootLogger();
+        	logger.info(new java.util.Date() + ": An error occured during the creation of the password-hash");
         	}
-    	return password;
+        catch (UnsupportedEncodingException e) {
+        	Logger logger = Logger.getRootLogger();
+    		logger.info(new java.util.Date() + ": An error occured during the creation of the password-hash");
+        }
+    	return password;*/
     }
 }

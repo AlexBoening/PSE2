447e64c2ed90a10f05f4004f80a1bb6fa58a173a
stephz <stephan.zurloh@gmx.de>
2013-12-15 14:06:40 +0100

    javadoc

--- PSE2 - icash/src/classes/Security.java
+++ PSE2 - icash/src/classes/Security.java
@@ -9,14 +9,27 @@ import java.security.NoSuchAlgorithmException;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.log4j.Logger;
 
+/**
+ * class for Security methods
+ * 
+ */
 public class Security {
 
+	/**
+	 * main program
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		String password = "geheim";
 		password = createPasswordHash(password);
 		System.out.println(password);
 	}
 	
+	/**
+	 * hashes the given password
+	 * @param password String
+	 * @return hashed password
+	 */
 	public static String createPasswordHash(String password) {
         	return DigestUtils.sha1Hex(password);
     }
